360e3d7672fb152d081a109811841f2ac131075b
Philipp Frenzel <philipp@frenzel.net>
2013-06-16 14:22:29 +0200

    cleaning the milk

--- yiiwymeditor/assets.php
+++ yiiwymeditor/assets.php
@@ -1,58 +1,10 @@
 <?php
 
 return array(
-	'yiidhtmlx/core' => array(
-		'sourcePath' => __DIR__ . '/assets',
+	'yiiwymeditor/core' => array(
+		'sourcePath' => __DIR__ . '/assets/wymeditor',
 		'js' => array(
-			'dhtmlxcommon.js',
+			'core.js',
 		),
-	),
-	'yiidhtmlx/chart' => array(
-		'sourcePath' => __DIR__ . '/assets/dhtmlxChart',
-		'js' => array(
-			'dhtmlxchart.js',
-		),
-		'css'=>array(
-			'dhtmlxchart.css'
-		),
-		'depends' => array('yiidhtmlx/core'),
-	),
-	'yiidhtmlx/menuObject' => array(
-		'sourcePath' => __DIR__ . '/assets/dhtmlxMenu',
-		'js' => array(
-			'dhtmlxmenu.js',
-			'ext/dhtmlxmenu_ext.js'
-		),
-		'css'=>array(
-			'skins/dhtmlxmenu_dhx_terrace.css'
-		),
-		'depends' => array('yiidhtmlx/core'),
-	),	
-	'yiidhtmlx/gridObject' => array(
-		'sourcePath' => __DIR__ . '/assets/dhtmlxGrid',
-		'js' => array(
-			'dhtmlxgrid.js',
-			'dhtmlxgridcell.js',
-			'ext/dhtmlxgrid_srnd.js',
-			'ext/dhtmlxgrid_filter.js',
-		),
-		'css'=>array(
-			'dhtmlxgrid.css',
-			'skins/dhtmlxgrid_dhx_terrace.css'
-		),
-		'depends' => array('yiidhtmlx/core'),
-	),
-	'yiidhtmlx/treeObject' => array(
-		'sourcePath' => __DIR__ . '/assets/dhtmlxTree',
-		'js' => array(
-			'dhtmlxtree.js',
-			'ext/dhtmlxtree_json.js',
-			'ext/dhtmlxtree_dragin.js',
-			'ext/dhtmlxtree_xw.js',
-		),
-		'css'=>array(
-			'dhtmlxtree.css',
-		),
-		'depends' => array('yiidhtmlx/core'),
 	)
 );
