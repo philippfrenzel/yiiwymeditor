d89d6cb3500fc1e8fb6d53feab73d2a846c17fba
Philipp Frenzel <philipp@frenzel.net>
2013-06-16 15:14:00 +0200

    fixing wrong asset

--- yiiwymeditor/assets.php
+++ yiiwymeditor/assets.php
@@ -4,14 +4,14 @@ return array(
 	'yiiwymeditor/core' => array(
 		'sourcePath' => __DIR__ . '/assets',
 		'js' => array(
-			'jquery.wymeditor.js',
-			'jquery.ui.resizable.js',
+			'jquery.wymeditor.js',			
 			'jquery-migrate-1.2.1.js',
 			'jquery.rdfquery.rdfa.min-1.0.js',			
 			'skins/default/skin.js',
 		),
 		'css' => array(
-			'skins/default/skin.css'
+			'skins/default/skin.css',
+			'jquery.ui.resizable.css',
 		)
 	)
 );
