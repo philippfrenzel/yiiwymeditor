3c4c3de288ce684ed9d197d72012b19e96edbcb2
Philipp Frenzel <philipp@frenzel.net>
2013-06-16 14:29:22 +0200

    adding needed assets

--- yiiwymeditor/assets.php
+++ yiiwymeditor/assets.php
@@ -5,6 +5,11 @@ return array(
 		'sourcePath' => __DIR__ . '/assets/wymeditor',
 		'js' => array(
 			'core.js',
+			'skins/default/skin.js',
+			'editor/base.js'
 		),
+		'css' => array(
+			'skins/default/skin.css'
+		)
 	)
 );
